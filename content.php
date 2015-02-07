4157772a18a42d91d1cd9284f461d2f8fc64bb63
Don Huntley <don.huntley@firethornedesign.com>
2015-02-07 10:46:29 -0800

    Customized and styled index pages

--- content.php
+++ content.php
@@ -5,12 +5,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="index-box">
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php firethorne_base_posted_on(); ?>
+                        <?php 
+                            if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) { 
+                                echo '<span class="comments-link">';
+                                comments_popup_link( __( 'Leave a comment', 'firethorne-base' ), __( '1 Comment', 'firethorne-base' ), __( '% Comments', 'firethorne-base' ) );
+                                echo '</span>';
+                            }
+                        ?>
+                        <?php edit_post_link( __( ' | Edit', 'firethorne-base' ), '<span class="edit-link">', '</span>' ); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -33,6 +42,7 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php firethorne_base_entry_footer(); ?>
+		
 	</footer><!-- .entry-footer -->
+    </div><!-- .index-box -->
 </article><!-- #post-## -->
