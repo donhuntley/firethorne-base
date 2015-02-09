886637015330b2e291dfbad9ba10d3cc2b69330f
Don Huntley <don.huntley@firethornedesign.com>
2015-02-09 08:28:48 -0800

    Highlight most recent post in index template

--- content.php
+++ content.php
@@ -5,15 +5,31 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="index-box">
-        <?php
+    
+        <?php 
+        if( $wp_query->current_post == 0 && !is_paged() && is_front_page() ) { // Custom template for the first post on the front page
             if (has_post_thumbnail()) {
-            echo '<div class="small-index-thumbnail clear">';
-            echo '<a href="' . get_permalink() . '" title="' . __('Click to read ', 'firethorne-base') . get_the_title() . '" rel="bookmark">';
-            echo the_post_thumbnail('index-thumb');
-            echo '</a>';
-            echo '</div>';
+                echo '<div class="front-index-thumbnail clear">';
+                echo '<div class="image-shifter">';
+                echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">';
+                echo the_post_thumbnail('large-thumb');
+                echo '</a>';
+                echo '</div>';
+                echo '</div>';
+            } 
+            echo '<div class="index-box';
+            if (has_post_thumbnail()) { echo ' has-thumbnail'; };
+            echo '">';
+        } else {
+            echo '<div class="index-box">';
+            if (has_post_thumbnail()) {
+                echo '<div class="small-index-thumbnail clear">';
+                echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">';
+                echo the_post_thumbnail('index-thumb');
+                echo '</a>';
+                echo '</div>';
             }
+        }
         ?>
 	<header class="entry-header">
             
@@ -23,7 +39,7 @@
                 echo '<i class="fa fa-thumb-tack sticky-post"></i>';
                 }
             ?>
-            
+
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
@@ -41,11 +57,22 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-             <?php the_excerpt(); ?>
-	<footer class="entry-footer continue-reading">
-            <?php echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'firethorne-base') . get_the_title() . '" rel="bookmark">Continue Reading<i class="fa fa-arrow-circle-o-right"></i></a>'; ?>
-        </footer><!-- .entry-footer -->
+        <?php 
+        if( $wp_query->current_post == 0 && !is_paged() && is_front_page() ) { 
+            echo '<div class="entry-content">';
+            the_content( __( '', 'my-simone' ) );
+            echo '</div>';
+            echo '<footer class="entry-footer continue-reading">';
+            echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">Read the article<i class="fa fa-arrow-circle-o-right"></i></a>'; 
+            echo '</footer><!-- .entry-footer -->';
+        } else { ?>
+            <div class="entry-content">
+            <?php the_excerpt(); ?>
+            </div><!-- .entry-content -->
+            <footer class="entry-footer continue-reading">
+            <?php echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'my-simone') . get_the_title() . '" rel="bookmark">Continue Reading<i class="fa fa-arrow-circle-o-right"></i></a>'; ?>
+            </footer><!-- .entry-footer -->
+        <?php } ?>
 		
     </div><!-- .index-box -->
 </article><!-- #post-## -->
